eae1a1d7e9224cbc2058c341a073663f69ae336d
Matty Strider <mattystrider@Mattys-MacBook-Air.local>
2024-09-12 21:29:57 -0400

    Removed lines with a variable called i.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
         FizzBuzz.main(new String[1]);
-        String i = "testing";
-        String i = "terminal push didn't work";
+
     }
 }
